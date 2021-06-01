1b2adf2fe0f476658c0488f152841da48d33d2c6
Cyberdom123 <dmichalczyk73@gamil.com>
2021-06-01 22:10:02 +0200

    ughbuga

--- esp8266/data/timer.js
+++ esp8266/data/timer.js
@@ -1,31 +1,7 @@
-    //Study counter
-    function Study() {
-      var StudyTime = document.getElementById("Study").value;
-
-      var tag_id = document.getElementById('counter');
-      tag_id.innerHTML = '<div id="time" data-animate="slide" style="font-size: 80px" data-role="countdown" data-on-alarm="Break()" data-hours="'+ StudyTime[0] + StudyTime[1] +'" data-minutes="'+ StudyTime[3] + StudyTime[4] +'" data-seconds="' + StudyTime[6] + StudyTime[7] + '"></div><br><p>Study</p>';
-    }
-    //Break counting
-    function Break() {
-      var BreakTime = document.getElementById("Break").value;
-
-      var tag_id = document.getElementById('counter');
-      tag_id.innerHTML = '<div  id="time" data-animate="slide" style="font-size: 80px" data-role="countdown"  data-hours="'+ BreakTime[0] + BreakTime[1] +'" data-minutes="'+ BreakTime[3] + BreakTime[4] +'" data-seconds="' + BreakTime[6] + BreakTime[7] + '"></div><br><p>Break</p>';
-    }
-    //Stop function
-    function Stop(timeleft) {
-      var lefthours = Math.floor(timeleft / 3600 );
-      var leftminutes = Math.floor((timeleft % 3600)/60);
-      var leftseconds = Math.floor(timeleft % 60);
-
-      var tag_id = document.getElementById('counter');
-      tag_id.innerHTML = '<div id="time" data-start="false" data-animate="slide" style="font-size: 80px" data-role="countdown" data-on-alarm="Break()" data-hours="'+ lefthours +'" data-minutes="'+ leftminutes +'" data-seconds="' + leftseconds + '"></div><br><p>Study</p>';
-    }
-
 // div time
   window.addEventListener('load', TakeTime);
   function TakeTime(){
-    var timetake = document.getElementById("test");
+    var timetake = document.getElementById("time");
 
     if(timetake.hasAttribute("data-hours")){
     var hours = timetake.getAttribute("data-hours");
@@ -50,20 +26,47 @@
 
     var Inseconds = hours * 3600 + minutes * 60 + seconds;
 
+    var button = document.getElementById("pausebutton").addEventListener("click", pause);
+    var button = document.getElementById("startbutton").addEventListener("click", start);
+    var button = document.getElementById("restartbutton").addEventListener("click", restart);
+
+    function pause(ispaused) {
+      if(ispaused){
+        paused = true;
+      }
+    }  
+    function start(reset) {
+      if(start && paused){
+        paused = false;
+      }
+    }
+    function restart(set){
+      if(restart){
+        var timetake = document.getElementById("time");
+        var datafield = document.getElementById("study");
+        timetake.setAttrubute("data-hours", )
+        timetake.setAttrubute("data-minutes", )
+        timetake.setAttrubute("data-seconds", )
+      }
+    }
 
     countdown();
     function countdown(){
       setTimeout(function(){
-      var hours = Math.floor(Inseconds / 3600); 
-      var minutes = Math.floor((Inseconds % 3600) / 60);
-      var seconds = Math.floor(Inseconds % 60);
+      if(paused){
+        timetake.innerHTML = time;
+        Inseconds++;
+      }else{
+        var hours = Math.floor(Inseconds / 3600);
+        var minutes = Math.floor((Inseconds % 3600) / 60);
+        var seconds = Math.floor(Inseconds % 60);
 
-      hours = hours < 10 ? '0' + hours : hours; //wow if in variablie awesome!
-      minutes = minutes < 10 ? '0' + minutes : minutes;
-      seconds = seconds < 10 ? '0' + seconds : seconds;
-      var time = (hours + ':' + minutes + ':' + seconds);
-      timetake.innerHTML = time;
-
+        hours = hours < 10 ? '0' + hours : hours; //wow if in variablie awesome!
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        time = (hours + ':' + minutes + ':' + seconds);
+        timetake.innerHTML = time;
+      }
       Inseconds--;
       if (Inseconds !== -1){
         countdown();
@@ -72,3 +75,5 @@
     }
 
   }
+
+// TimePicker form
